8477b68bb38dfb0f4aaa3ad7d19c625f374e53f2
DELL <DELL@DESKTOP-ADPCQLP>
2024-01-31 16:31:26 -0300

    mais um pouco

--- curso-jdev-treinamento/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ curso-jdev-treinamento/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,13 +1,19 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 /**
@@ -18,23 +24,36 @@ import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 public class GreetingsController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
-    
-    
-    @RequestMapping (value = "/olamundo/{nome}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String retornaOlaMundo (@PathVariable String nome) {
-    	return "Olá mundo " + nome;
-    }
-    
-    public Respons
+
+	/**
+	 *
+	 * @param name the name to greet
+	 * @return greeting text
+	 */
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String retornaOlaMundo(@PathVariable String nome) {
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+
+		usuarioRepository.save(usuario); // grava no banco de dados
+
+		return "Olá mundo " + nome;
+
+	}
+
+	
+	@GetMapping(value = "listatodos")//primeiro metodo da api, listando todos
+	@ResponseBody // retorna os dados para o corpo da resposta
+	
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+		List<Usuario> usuarios	= usuarioRepository.findAll(); // executa a consulta no banco de dados
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);//retorna a lista em JSON
+	}
 }
